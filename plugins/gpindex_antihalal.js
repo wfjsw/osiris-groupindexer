c65e606c989a1ad1738d508ae7e921e9ac8c09f8
wfjsw <wfjsw@users.noreply.github.com>
2018-09-07 17:11:54 +0800

    update

--- plugins/gpindex_antihalal.js
+++ plugins/gpindex_antihalal.js
@@ -1,16 +1,19 @@
 const langCodeBanned = new Set(['fa-IR', 'fa'])
 const halal_display_name = /sharma|pollsciemo|amarhs|moham|ali.*reza|ahmed/i
+const halal_keyboard_match = /@TGNumberBot/i
 const max_halal_char_threshold = 12
 const max_halal_pct_threshold = 0.45
 const send_capture_threshold = 120
 const send_notification_threshold = 120
 const kick_wait_threshold = 30
+const cancel_keyboard_threshold = 15
 
 const antihalal_manager = -1001159383809
 const chan_capture = -1001135234856
 var _e, comlib
 let last_sent_group = new Map()
 let last_sent_capture = new Map()
+let last_sent_cancelkeyboard = new Map()
 let last_kicked = new Map()
 let sticker_pack_name_cache = new Map()
 let tempwhitelist = new Map()
@@ -96,9 +99,30 @@ function gcArray() {
     for (let [k, v] of last_sent_capture)
         if (d - v > send_capture_threshold)
             last_sent_capture.delete(k)
+    for (let [k, v] of last_sent_cancelkeyboard)
+        if (d - v > cancel_keyboard_threshold)
+            last_sent_cancelkeyboard.delete(k)
     for (let [k, v] of last_kicked)
         if (d - v > kick_wait_threshold)
             last_kicked.delete(k)
+}
+
+const additional_ruleset = {
+    'sharma': (msg) => {
+        let rgx = /sharma|dev|开发|下马|amrahs|ved/i
+        let uname = msg.from.first_name + msg.from.last_name
+        if (uname.match(rgx)) return true
+        return false
+    }
+}
+
+async function execAdditionalRuleset(msg) {
+    for (let rs in additional_ruleset) {
+        let is_used = await comlib.GroupExTag.queryGroupExTag(msg.chat.id, `feature:antihalal.ruleset.${rs}`)
+        if (!is_used) continue
+        let result = additional_ruleset[rs](msg)
+        if (result) return true
+    }
 }
 
 async function privateLanguageDetection(msg, bot) {
@@ -136,7 +160,7 @@ async function examineDisplayName(msg, new_member, bot) {
     let inviter_display_name = msg.from.first_name + (msg.from.last_name || '') + inviter_desc
     let newmember_desc = (await bot.getChat(new_member.id)).description || ''
     let display_name = new_member.first_name + (new_member.last_name || '') + newmember_desc
-    const is_kick_halal_name = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalalenhanced')
+    const is_kick_halal_name = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalal.name')
     let is_halal = testHalal(inviter_display_name) || testHalal(display_name) || !!(display_name + (new_member.username || '')).match(halal_display_name) || langCodeBanned.has(new_member.language_code) || langCodeBanned.has(msg.from.language_code)
     if (is_halal) {
         let is_nocapture = !!(await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'nocapture'))
@@ -155,19 +179,19 @@ async function examineSticker(msg, bot) {
     let display_name = msg.from.first_name + (msg.from.last_name || '')
     let stickerset = msg.sticker.set_name
     let stickersetname = sticker_pack_name_cache.get(stickerset) || (await bot.getStickerSet(stickerset)).title
-    const is_enhanced = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalalenhanced')
+    const [is_sticker, is_name] = await comlib.GroupExTag.queryGroupExTag(msg.chat.id, ['feature:antihalal.sticker', 'feature:antihalal.name'])
     sticker_pack_name_cache.set(stickerset, stickersetname)
     let is_halal = testHalal(stickersetname) || langCodeBanned.has(msg.from.language_code)
-    let is_halalenhanced = testHalal(display_name + stickersetname)
+    let is_halalname = testHalal(display_name + stickersetname)
     if (is_halal) {
         await uploadHalal(msg, bot)
-        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()))
+        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()) && is_sticker)
             await kickByHalal(msg.chat, msg.from, false, bot)
         return true
     }
-    if (is_halalenhanced) {
+    if (is_halalname) {
         await uploadHalal(msg, bot)
-        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()) && is_enhanced) {
+        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()) && is_sticker && is_name) {
             await kickByHalal(msg.chat, msg.from, false, bot)
             return true
         }
@@ -176,7 +200,7 @@ async function examineSticker(msg, bot) {
 }
 
 async function examineNormalMsg(msg, bot) {
-    const is_enhanced = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalalenhanced')
+    const is_name = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalal.name')
     let need_test = ''
     let display_name = msg.from.first_name + (msg.from.last_name || '')
     let is_halal = langCodeBanned.has(msg.from.language_code)
@@ -202,7 +226,7 @@ async function examineNormalMsg(msg, bot) {
     }
     if (is_halal_name) {
         await uploadHalal(msg, bot)
-        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()) && is_enhanced) {
+        if (!tempwhitelist.has(msg.chat.id.toString() + msg.from.id.toString()) && is_name) {
             await kickByHalal(msg.chat, msg.from, false, bot)
             return true
         }
@@ -211,7 +235,8 @@ async function examineNormalMsg(msg, bot) {
 }
 
 async function kickByHalal(group, user, is_flag, bot) {
-    const is_halal_process_enabled = !!await comlib.GroupExTag.queryGroupExTag(group.id, 'feature:antihalal')
+    const [is_halal_process_enabled, is_nodb_enabled] = await comlib.GroupExTag.queryGroupExTag(group.id, ['feature:antihalal', 'feature:antihalal.nodb'])
+    if (is_nodb_enabled && is_flag) return
     if (is_halal_process_enabled) {
         let usermsg = `清真用户：<a href="tg://user?id=${user.id}">${user.first_name || ''}`
         if (user.last_name) usermsg += ` ${user.last_name || ''}`
@@ -261,9 +286,32 @@ async function kickByHalal(group, user, is_flag, bot) {
     }
 }
 
+async function removeKeyboard(msg, bot) {
+    const [is_halal_process_enabled] = await comlib.GroupExTag.queryGroupExTag(msg.chat.id, ['feature:antihalal'])
+    if (!is_halal_process_enabled) return false 
+    if (last_sent_cancelkeyboard.has(msg.chat.id.toString())) 
+        if (Math.floor(Date.now() / 1000) - last_sent_group.get(msg.chat.id.toString()) <= cancel_keyboard_threshold)
+            return false
+    last_sent_cancelkeyboard.set(msg.chat.id.toString(), Math.floor(Date.now() / 1000))
+    let cclmsg = await bot.sendMessage(msg.chat.id, '检测到清真键盘，已将键盘恢复到正常状况。3 秒钟后该消息会自行删除。', {
+        reply_markup: {
+            hide_keyboard: true
+        }
+    })
+    setTimeout((m) => {
+        bot.deleteMessage(m.chat.id, m.message_id)
+            .catch(() => { })
+    }, 3 * 1000, cclmsg)
+ }
+
 async function preProcessStack(msg, bot) {
     try {
-        const non_halal = !!await comlib.UserFlag.queryUserFlag(msg.from.id, 'nothalal')
+        if (msg.text) {
+            if (msg.text.match(halal_keyboard_match)) {
+                await removeKeyboard(msg, bot)
+            }
+        }
+        const non_halal = await comlib.UserFlag.queryUserFlag(msg.from.id, 'nothalal')
         if (non_halal) return
         let is_halal
         if (msg.chat.id > 0) {
@@ -289,6 +337,7 @@ async function preProcessStack(msg, bot) {
                 is_halal = await examineNormalMsg(msg, bot) || is_halal
             }
         }
+        if (!is_halal && msg.chat.id < 0) is_halal = await execAdditionalRuleset(msg)
         if (is_halal) {
             const is_halal_process_enabled = !!await comlib.GroupExTag.queryGroupExTag(msg.chat.id, 'feature:antihalal')
             if (is_halal_process_enabled) {
@@ -317,13 +366,11 @@ async function processCallbackQuery(msg, type, bot) {
                         chat_id: msg.message.chat.id,
                         message_id: msg.message.message_id,
                     })
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '已经上交给了真主安拉。'
                     })
                 } else {
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '你不是老阿訇。'
                     })
                 }
@@ -334,13 +381,11 @@ async function processCallbackQuery(msg, type, bot) {
                         chat_id: msg.message.chat.id,
                         message_id: msg.message.message_id,
                     })
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '好的，这不清真。'
                     })
                 } else {
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '你不是老阿訇。'
                     })
                 }
@@ -356,13 +401,11 @@ async function processCallbackQuery(msg, type, bot) {
                     try {
                         await bot.unbanChatMember(msg.message.chat.id, uid)
                     } catch (e) {}
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '好的，这不清真。'
                     })
                 } else {
-                    return bot.answerCallbackQuery({
-                        callback_query_id: msg.id,
+                    return bot.answerCallbackQuery(msg.id, {
                         text: '你不是阿訇。'
                     })
                 }
